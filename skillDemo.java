1d8effed21f983c61381e68d6d5044a9c84336af
Mashyuf <matthew919fang@gmail.com>
2022-05-01 23:30:12 -0700

    Updated SkillDemo

--- skillDemo.java
+++ skillDemo.java
@@ -1,1 +1,32 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
+public class skillDemo {
+
+    /**
+     * Print all elements starting from inputted index
+     * 
+     * @param index
+     */
+    public static ArrayList<String> printElements(String[] array, int idx) {
+        ArrayList<String> toReturn = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            toReturn.add(array[i]);
+        }
+        return toReturn;
+    }
+
+    public static void main(String[] args) throws IOException {
+        List<String> input = new ArrayList<String>();
+        Path fileName = Path.of("test-file.md");
+
+        input = Files.readAllLines(fileName);
+
+        String[] array = input.toArray(new String[0]);
+
+        System.out.print(printElements(array, 0));
+    }
+}
---
+++ skillDemoTest.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class skillDemoTest {
+
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+    
+    @Test
+    public void testFileIndex0() throws IOException {
+        List<String> input = new ArrayList<String>();
+        Path fileName = Path.of("C:\\Users\\Matt\\Documents\\CSE15L\\test-file.md");
+        input = Files.readAllLines(fileName);
+        String[] array = input.toArray(new String[0]);
+
+        assertEquals("[1, 2, 3, 4, 5]", skillDemo.printElements(array, 0).toString());
+    }
+
+    @Test
+    public void testFileIndex1() throws IOException {
+        List<String> input = new ArrayList<String>();
+        Path fileName = Path.of("C:\\Users\\Matt\\Documents\\CSE15L\\test-file.md");
+        input = Files.readAllLines(fileName);
+        String[] array = input.toArray(new String[0]);
+
+        assertEquals("[2, 3, 4, 5]", skillDemo.printElements(array, 1).toString());
+    }
+}
